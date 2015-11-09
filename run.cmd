75ef1988d0f03bd26dd9511b6e40eff6dca3b4c6
Chanh Nguyen <chanh75@gmail.com>
2015-11-09 15:33:10 -0700

    Updated to execute Maven instead of raw Java.

--- run.cmd
+++ run.cmd
@@ -1,19 +1,11 @@
+REM
+REM This example runs data scraping for Google stock.
+REM
+REM Requires Maven
+REM
+
 setlocal
 
-SET MVN_LIB=.m2\repository
-SET COMMONS_LANG=%MVN_LIB%\org\apache\commons\commons-lang3\3.0\commons-lang3-3.0.jar
-SET COMMONS_DB=%MVN_LIB%\commons-dbutils\commons-dbutils\1.5\commons-dbutils-1.5.jar
-SET COMMONS_IO=%MVN_LIB%\commons-io\commons-io\2.4\commons-io-2.4.jar
-SET MYSQL_CONN=%MVN_LIB%\mysql\mysql-connector-java\5.1.25\mysql-connector-java-5.1.25.jar
-SET JSOUP=%MVN_LIB%\org\jsoup\jsoup\1.8.3\jsoup-1.8.3.jar
-
-java -classpath .\target\classes;^
-..\DataWarehouse\target\classes;^
-%COMMONS_LANG%;^
-%COMMONS_DB%;^
-%MYSQL_CONN%;^
-%COMMONS_IO%;^
-%JSOUP%^
- com.mohawk.webcrawler.Main %1 %2
+mvn exec:java -Dexec.mainClass="com.mohawk.webcrawler.Main" -Dexec.args="-d -s .\yahoo_finance_stocks.txt -v {SYMBOL:\"GOOG\"}"
 
 endlocal
